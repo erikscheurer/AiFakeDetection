9da7d2acbc9253da71605a98fff6b6d7000f16c3
Erik Scheurer <erik.scheurer@gmx.de>
2024-05-09 17:04:57 +0200

    Add first dummy dataset

---
+++ data/readme.md
@@ -0,0 +1,22 @@
+# put the data here
+Structure as in the [google drive](https://drive.google.com/drive/folders/1jGt10bwTbhEZuGXLyvrCuxOI0cBqQ1FS?usp=sharing)
+- GenImage
+    - ADM
+        - train
+            - ai
+            - nature
+    - BigGAN
+    - stable_diffusion_1.4
+    - ...
+- TinyGenImage
+    - imagenet_biggan...
+        - train
+            - ai
+            - nature
+        - val
+            - ai
+            - nature
+    - stable_diffusion
+        - ...
+
+    
---
+++ datasets/dummy.py
@@ -0,0 +1,21 @@
+# This is supposed to be a dummy dataset for quick prototyping
+
+import torch
+from torch.utils.data import Dataset
+
+class DummyDataset(Dataset):
+    def __init__(self, n_samples=1000, shape=(3, 32, 32), n_classes=2):
+        self.n_samples = n_samples
+        self.shape = shape
+        self.n_classes = n_classes
+        self.data = torch.randn(n_samples, *shape)
+        self.targets = torch.randint(0, n_classes, (n_samples,))
+
+    def __len__(self):
+        return self.n_samples
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.targets[idx]
+
+    def __repr__(self):
+        return f"DummyDataset(n_samples={self.n_samples}, shape={self.shape}, n_classes={self.n_classes})"
